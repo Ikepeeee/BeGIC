e7e80b9a93b0e1883a53dc8d76142ab357e6a69a
Ikepeeee <toru48@outlook.jp>
2016-07-22 12:22:44 +0900

    color

--- src/java/jp/tolz/begic/prototype/interpreter/values/VColor.java
+++ src/java/jp/tolz/begic/prototype/interpreter/values/VColor.java
@@ -3,7 +3,7 @@ package jp.tolz.begic.prototype.interpreter.values;
 import jp.tolz.begic.prototype.interpreter.values.base.Values;
 
 public class VColor extends Values {
-	static VColor RED, BLUE, YELLOW, GREEN;
+	static VColor RED, BLUE, YELLOW, GREEN, BLACK, WHITE;
 	
 	public VColor(int r, int g, int b) {
 

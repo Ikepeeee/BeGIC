acbd235898d57e8ac73631c366bc91b9978afa8c
Ikepeeee <toru48@outlook.jp>
2017-03-26 22:30:32 +0900

    パッケージ名の変更 前: jp.tolz.begic.prototype.* 後: jp.begic.*

---
+++ src/java/jp/begic/api/BegicAPI.java
@@ -0,0 +1,14 @@
+package jp.begic.api;
+
+import jp.begic.api.model.ITokenList;
+
+public class BegicAPI {
+	public static BegicAPI getBegicAPI(){
+		return null;
+	}
+	
+	public ITokenList getTokenList(){
+		return null;
+	}
+	
+}

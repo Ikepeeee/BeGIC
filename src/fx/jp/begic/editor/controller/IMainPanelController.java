bb07a32463425c4b7a75c8a7c87865e30aabbf63
Ikepeeee <toru48@outlook.jp>
2017-04-24 22:56:56 +0900

    fxml原型

--- src/fx/jp/begic/editor/controller/IMainPanelController.java
+++ src/fx/jp/begic/editor/controller/IMainPanelController.java
@@ -3,12 +3,15 @@ package jp.begic.editor.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public interface MainPanelControllerImpl {
+public interface IMainPanelController {
 	@FXML
 	public void newFile(ActionEvent e);
 	
 	@FXML
 	public void openFile(ActionEvent e);
+	
+	@FXML
+	public void saveFile(ActionEvent e);
 	
 	@FXML
 	public void quit(ActionEvent e);
--- src/fx/jp/begic/editor/controller/MainPanelController.java
+++ src/fx/jp/begic/editor/controller/MainPanelController.java
@@ -1,25 +1,33 @@
 package jp.begic.editor.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
-public class MainPanelController implements MainPanelControllerImpl {
-
+public class MainPanelController implements IMainPanelController {
+	@FXML
+	private TextArea codeTextArea;
+	
 	@Override
 	public void newFile(ActionEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		
 	}
 
 	@Override
 	public void openFile(ActionEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-
+	}
+	
+	@Override
+	public void saveFile(ActionEvent e){
+		System.out.println(codeTextArea.getText());
 	}
 
 	@Override
 	public void quit(ActionEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		// 終了の方法は検討
+		System.exit(0);
 	}
 
 }

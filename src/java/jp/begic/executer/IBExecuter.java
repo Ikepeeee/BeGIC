ac91656fca0abc2e62ab9476b81e604ec843bc3a
Toru Ikeda <toru48@outlook.jp>
2017-11-25 22:58:04 +0900

    REPL(Read Eval Print Loop)の追加

--- src/java/jp/begic/executer/IBExecuter.java
+++ src/java/jp/begic/executer/IBExecuter.java
@@ -1,20 +1,51 @@
 package jp.begic.executer;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import jp.begic.interpreter.parser.BegicParser;
+import jp.begic.interpreter.parser.ParseException;
+import jp.begic.interpreter.process.BegicVisitor;
 
 /**
- * BeGIC実行者です。
- * 複数回の実行を行います。
+ * BeGIC実行者です。 複数回の実行を行います。
+ * 
  * @author Toru Ikeda
  *
  */
 public class IBExecuter implements Executer {
 
 	@Override
-	public void exec(InputStream code) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+	public void exec(InputStream stream) {
+
+		BegicVisitor visitor = new BegicVisitor();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			while (true) {
+				String line = null;
+				System.out.print("begic > ");
+				try {
+					line = br.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(0);
+				}
+
+				if (line.matches("exit.*"))
+					break;
+				BegicParser parser = new BegicParser(new ByteArrayInputStream(
+						line.getBytes()));
+				parser.Program().jjtAccept(visitor, null);
+			}
+		} catch (ParseException e) {
+			System.err.println("構文エラー");
+			e.printStackTrace();
+			System.exit(0);
+		}
+
 	}
 
 }
--- src/java/jp/begic/interpreter/IBegic.java
+++ src/java/jp/begic/interpreter/IBegic.java
@@ -1,12 +1,46 @@
 package jp.begic.interpreter;
 
+import jp.begic.executer.Executer;
+import jp.begic.executer.IBExecuter;
+
 /**
  * 即時実行用のクラスです。
+ * 
  * @author Toru Ikeda
  *
  */
 public class IBegic {
-	public static void main(String[] args){
+	private static final String releaseDate = "2017/11/25";
+	private static final String version = "1.0.0";
+	private static final String buildBy = "jdk1.8.0_91";
+	private static final String name = "BeGIC";
+
+	public static void main(String[] args) {
 		
+		if (args.length > 0 && args[0].matches("-.*")) {
+			switch (args[0]) {
+			case "-v":
+			case "--version":
+				System.out.println(version());
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Unrecognized option: " + args[0]);
+				break;
+			}
+		} else {
+			new IBegic();
+		}
+	}
+
+	public IBegic() {
+		Executer exe = new IBExecuter();
+		
+		exe.exec(System.in);
+	}
+
+	public static String version() {
+		return name + " version " + version + " (" + releaseDate + ") ["
+				+ buildBy + "]";
 	}
 }

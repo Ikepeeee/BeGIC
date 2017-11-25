1761825ac4c72467c2c8bd08d2612ce49f54a400
Toru Ikeda <toru48@outlook.jp>
2017-11-25 22:41:48 +0900

    paint関数を抜けることができるように変更

--- src/java/jp/begic/interpreter/canvas/BCanvas.java
+++ src/java/jp/begic/interpreter/canvas/BCanvas.java
@@ -3,8 +3,8 @@ package jp.begic.interpreter.canvas;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Deque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import javax.swing.JFrame;
 
@@ -18,31 +18,49 @@ import jp.begic.interpreter.commands.base.BDrawCommand;
  */
 public class BCanvas extends Canvas {
 	private JFrame frame = null;
-	private Queue<BDrawCommand> drawList = new LinkedBlockingQueue<>(1);
-	
+	private Deque<BDrawCommand> drawList = new LinkedBlockingDeque<>();
+	private boolean ret = false;
+
 	public BCanvas(JFrame frame) {
 		this.frame = frame;
 		this.setBackground(Color.WHITE);
 	}
-	
+
 	public void push(BDrawCommand cmd) {
-		while(!drawList.offer(cmd));
+		while (!drawList.offer(cmd))
+			;
 	}
 	
+	public void unshift(BDrawCommand cmd) {
+		drawList.addFirst(cmd)
+			;
+	}
+	
+	/**
+	 * 今の描写処理が終了後、paint関数を抜け出します。
+	 */
+	public synchronized void returnPaint(boolean b){
+		ret = b;
+	}
+
 	private BDrawCommand pop() {
 		BDrawCommand cmd = null;
-		while((cmd = drawList.poll()) == null);
+		while ((cmd = drawList.poll()) == null)
+			;
 		return cmd;
 	}
 
 	@Override
 	public void paint(Graphics g) {
+		ret = false;
 		g.setColor(Color.black);
 		// コマンドがキャンバスを編集できる情報を提供
 		BDrawCommand.set(frame, this, g);
 		while (true) {
 			BDrawCommand cmd = pop();
 			cmd.draw();
+			if(ret)
+				break;
 		}
 
 	}
